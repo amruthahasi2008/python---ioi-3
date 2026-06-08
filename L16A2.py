e27b2c66be99742567e24722b7cb58a86d122684
Sri Sai Amrutha Hasini Adunutala <amruthahasini2008@gmail.com>
2026-06-08 11:44:37 +0530

    Add files via upload

---
+++ L16A2.py
@@ -0,0 +1,7 @@
+def print_f(num):
+    print("The factors of the  ",num,"are:")
+    for i in range(1, num+1):
+        if num % i == 0 :
+            print(i)
+num = int(input("enter your number"))
+print_f(num)
